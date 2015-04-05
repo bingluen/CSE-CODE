673e1f9a787b842a8a9a053e531fa13cdf42d50e
Erickson <a820224@gmail.com>
2015-04-06 00:53:53 +0800

    add test data & function

--- OS-PROG1/main.cpp
+++ OS-PROG1/main.cpp
@@ -3,8 +3,13 @@
 
 using namespace std;
 
-void loadMap(char& , char &[2][20][20]);
-void robot(char char &[2][20][20])
+int direction[4][2] = {{0, -1}, {1, 0}, {0 ,1}, {-1, 0}};
+
+
+void loadMap(char * , char [2][20][20]);
+bool isSingleRoute(char [20][20], unsigned int[2]);
+int& findStartPoint(char [20][20]);
+
 
 int main(int argc, char* argv[])
 {
@@ -15,20 +20,22 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	char map [2][20][20]
+	char map [2][20][20];
 	loadMap(argv[1], map);
+	int &startPoint = (findStartPoint(map[0]));
+	cout << startPoint << endl;
 
+	return 0;
 
 }
 
-void loadMap(char& filename, char &map[2][20][20])
+void loadMap(char *filename, char map[2][20][20])
 {
 	ifstream mapFile(filename, ios::in);
 
-	if ( !inFile )
+	if ( !mapFile )
 	{
 		cout << "File could not be opened" << endl;
-		fgetc(stdin);
 		exit( 1 );
 	}
 
@@ -36,14 +43,31 @@ void loadMap(char& filename, char &map[2][20][20])
 	unsigned int row = 0;
 	while (mapFile.getline(input, 20))
 	{
+		cout << input << endl;
 		if(strlen(input))
 		{
 			strcpy(map[0][row++], input);
 			strcpy(map[1][row++], input);
 		}
-			
 	}
 
 	mapFile.close();
 }
 
+
+int &findStartPoint(char map[20][20])
+{
+	for(unsigned int i = 0; i < 20; i++)
+	{
+		for(unsigned int j = 0; j < 20; j++)
+		{
+			if(map[i][j] == 'S' || map[i][j] == 's')
+			{
+				int *position = new int[2];
+				position[0] = i;
+				position[1] = j;
+				return *position;
+			}
+		}
+	}
+}
