8ba6274e6d89b5a75292ac2fcf597179c62211a8
Ercikson <a820224@gmail.com>
2015-04-14 20:01:36 +0800

    try to implement explore

--- OS-PROG1/main.cpp
+++ OS-PROG1/main.cpp
@@ -1,21 +1,39 @@
+#include <cstring>
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
 
 using namespace std;
 
+
+//direction of robot
 int direction[4][2] = {{0, -1}, {1, 0}, {0 ,1}, {-1, 0}};
 
 
 void loadMap(char * , char [2][20][20]);
-int route(char [20][20], int[2]);
+int route(char [20][20], int[2], bool[4]);
 int* findStartPoint(char [20][20]);
-void robot(int, int , char[][20][20]);
+//void explore();
 
+
+//map
+char map [2][20][20];
+
+//robot
+struct robot{
+    int pos_x;
+    int pos_y;
+    int direction;
+};
 
 int main(int argc, char* argv[])
 {
+    struct timespec begin, end;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
 	if(argc <= 1)
 	{
 		cout << "Please assign a map." << endl;
@@ -23,11 +41,8 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
-	char map [2][20][20];
 	loadMap(argv[1], map);
-	int *startPoint = (findStartPoint(map[0]));
-	robot(startPoint[0], startPoint[1], map);
-
+	
 	return 0;
 
 }
@@ -50,9 +65,8 @@ void loadMap(char *filename, char map[2][22][22])
 		if(strlen(input))
 		{
 			//cout << " " << input;
-			map[0][row][0] = map[1][row][0] = map[0][row][21] = map[1][row][21] = '*';
-			strcpy(map[0][row][1], input);
-			strcpy(map[1][row++][1], input);
+			strcpy(map[0][row], input);
+			strcpy(map[1][row++], input);
 		}
 		//cout << endl;
 	}
@@ -80,35 +94,45 @@ int* findStartPoint(char map[20][20])
 	return 0;
 }
 
-int route(char map[20][20], int pos[2])
+int route(char map[20][20], int pos[2], bool available[4])
 {
 	int count = 0;
 	for(size_t i = 0; i < 4; i++)
 	{
 		if(map[pos[0] + direction[i][0]][pos[1] + direction[i][1]] == ' ')
-			count++;
+        {
+            count++;
+            available[i] = true;
+        }else{
+            available[i] = false;
+        }
 	}
 
 	return count;
 }
 
-void robot(int x, int y, char map[][20][20])
+void explore()
 {
-	int pos[2];
-	pos[0] = x, pos[1] = y;
+    int *startPoint = (findStartPoint(map[0]));
+    int numRoute;
+    bool available[4];
+    while(numRoute = route(map[0], startPoint, available))
+    {
+        //Only one route
+        if(numRoute == 1)
+        {
+            //set currently position be wall
+            map[0][startPoint[0]][startPoint[1]] = '*';
+            int i = 0;
+            for(; i < 4 && !available[i];i++);
+            startPoint[0] += direction[i][0];
+            startPoint[1] += direction[i][1];
+        }
 
-	int numRoute = 0;
-
-	while((numRoute = route(map[0], pos[0], pos[1])) > 0)
-	{
-		//路線數量大於1
-		if(numRoute > 1)
-		{
-			for(size_t i = 0; i < numRoute; i++)
-				fork();
-		}
-
-
-		
-	}
+        //multi route
+        if(numRoute > 1)
+        {
+            //multi route
+        }
+    }
 }
