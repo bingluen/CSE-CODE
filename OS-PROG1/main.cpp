1e1859fe778851692cd3a68c994b747b76fb26b6
Erickson Juang <a820224@gmail.com>
2015-04-15 15:35:47 +0800

    failed version -1

--- OS-PROG1/main.cpp
+++ OS-PROG1/main.cpp
@@ -15,7 +15,12 @@ int getRoute(char [20][20], bool *, int*);
 void findStartPoint(char *[20], int*);
 void explore();
 
-int createProcess(int &count, int process_number);
+void robot(int numRoute,  int pos[]);
+
+struct single
+{
+	bool isFound;
+};
 
 //direction of robot
 int direction[4][2] = {{0, -1}, {1, 0}, {0 ,1}, {-1, 0}};
@@ -84,127 +89,26 @@ void findStartPoint(char map[20][20], int* position)
 
 void explore()
 {
-	//save robot position
-	int pos[2];
 
-	//save route num
-	int numRoute = 0;
+	//save position
+	int pos[2] = { 0 };
 
-	//save direction is passable
-	bool passable[4] = { 0 };
-
-	//save isFound?
-	bool isFound = false;
-
-	//step1 find start position
+	//get start position
 	findStartPoint(map, pos);
 
-	pid_t pid = 0;
+	/* For every robot */
+
+	//save direction passable
+	bool passable[4] = { 0 };
+	
+	//save number of Route
+	int numRoute = 0;
 
 	//起點
 	cout << "[pid = " << getpid() << "]" << "(" << pos[0] << " ," << pos[1] << ")" << endl;
-	
-	if(pid < 0)
-	{
-		cout << "fork 出錯了QQ" << endl;
-		exit(1);
-	}
-	else if(pid == 0)
-	{
-		//step3 child process 進行探險
-		//有路才走~
-		while((numRoute = getRoute(map, passable, pos)) > 0
-			&& !(isFound = (map[pos[0]][pos[1]] == 'K' || map[pos[0]][pos[1]] == 'k' )))
-		{
-			int count = 0;
-			printf("pid = %d, num = %d pos = (%d, %d)\n", getpid(), numRoute, pos[0], pos[1]);
-			if(numRoute > 1)
-			{
-				//多條路就fork
-				
-				pid = createProcess(count, numRoute);
-				if(pid == 0)
-				{
-					//fork後把自己印出來
-					cout << "[pid = " << getpid() << "]" << "(" << pos[0] << " ," << pos[1] << ")" << endl;
 
-					//再繼續走
-					size_t i = 0;
-					for(; i < 4 && !passable[i]; i++);
-					passable[i] = false;
-					//把原本的路堵死變牆壁
-					map[pos[0]][pos[1]] = '*';
-					//前進
-					pos[0] += direction[i][0];
-					pos[1] += direction[i][1];
-
-					//檢查有沒有道牆外..
-					if(pos[0] > 19 || pos[1] > 19)
-					{
-						//退回去
-						pos[0] -= direction[i][0];
-						pos[1] -= direction[i][1];
-					}
-				} else if(pid > 0) {
-					for(size_t i = 0; i < 4; i++)
-						passable[i] = false;
-					waitpid(-1, NULL, 0);
-				}
-				
-			}
-			else
-			{
-				size_t i = 0;
-				for(; i < 4 && !passable[i]; i++);
-				passable[i] = false;
-				//把原本的路堵死變牆壁
-				map[pos[0]][pos[1]] = '*';
-				//前進
-				pos[0] += direction[i][0];
-				pos[1] += direction[i][1];
-
-				//檢查有沒有道牆外..
-				if(pos[0] > 19 || pos[1] > 19)
-				{
-					//退回去
-					pos[0] -= direction[i][0];
-					pos[1] -= direction[i][1];
-					break;
-				}
-
-			}
-			sleep(2);
-		}
-
-		//step4 沒路了回傳訊息
-		// Child process closes up input side of pipe //
-        close(fd[0]);
-        // Send "string" through the output side of pipe //
-        char messages[80];
-        if(isFound)
-        {
-        	sprintf(messages, "%d (%d, %d) Found !", getpid(), pos[0], pos[1]);
-        	write(fd[1], messages, sizeof(messages));
-        }
-        else
-        {
-        	sprintf(messages, "%d (%d, %d) None !", getpid(), pos[0], pos[1]);
-        	write(fd[1], messages, sizeof(messages));
-        }
-        exit(0);
-	}
-	else
-	{
-		//把水管一端關起來
-		close(fd[1]);
-		//等所有的child
-		while (pid = waitpid(-1, NULL, 0)) {
-			//有child結束了，應該要有訊息回來，把他印出來
-			nbytes = read(fd[0], tubeBuffer, sizeof(tubeBuffer));
-			cout << tubeBuffer << endl;
-		}
-	}
-	
+	//讓robot 行動
+	robot(getRoute(map, passable, pos), pos);
 }
 
 int getRoute(char map[20][20], bool *passable, int *pos)
@@ -225,20 +129,104 @@ int getRoute(char map[20][20], bool *passable, int *pos)
 	return count;
 }
 
-int createProcess(int &count, int process_number) {
-	
+void robot(int numRoute, int pos[])
+{
+	//is fonud ?
+	bool isFound = false;
+
+	//來fork一隻機器人前進
 	pid_t pid = fork();
-	
-	if ( pid < 0 ) {
-		cout << "fork() error\n";
-		exit(1);
+
+	if(pid == 0)
+	{
+		//起點
+		cout << "[pid = " << getpid() << "]" << "(" << pos[0] << " ," << pos[1] << ")" << endl;
+
+		//save direction passable
+		bool passable[4] = { 0 };
+
+		//只有一條路
+		if(numRoute == 1)
+		{
+			
+			//前進條件
+			//1.只有一條路
+			//AND
+			//2.沒找到礦石
+			while((numRoute = getRoute(map, passable, pos)) == 1
+				&& !(isFound = (map[pos[0]][pos[1]] == 'K' || map[pos[0]][pos[1]] == 'k' )))
+			{
+				//把原本的路堵死變牆壁
+				map[pos[0]][pos[1]] = '*';
+
+				//找方向
+				size_t i = 0;
+				for(; i < 4 && !passable[i]; i++);
+
+				//前進
+				pos[0] += direction[i][0];
+				pos[1] += direction[i][1];
+
+				//檢查有沒有道牆外..
+				if(pos[0] > 19 || pos[1] > 19)
+				{
+					//退回去
+					pos[0] -= direction[i][0];
+					pos[1] -= direction[i][1];
+					break;
+				}
+			}
+
+			//不前進的原因
+			//1.找到了
+			//OR
+			//2.有岔路
+			//OR
+			//3.死路了
+			if(isFound)
+			{
+				cout << getpid() << " (" << pos[0] << ", " << pos[1] << ") Found !" << endl;
+			}
+			else if(numRoute == 0)
+			{
+				//死路狀態
+				//寫下訊息
+				cout << getpid() << " (" << pos[0] << ", " << pos[1] << ") None !" << endl;
+				
+			}
+			else if(numRoute > 1)
+			{
+				for(size_t i = 0; i < 4; i++)
+				{
+					if(passable[i])
+					{
+						passable[i] = false;
+						int newPos[2];
+						newPos[0] = pos[0] + direction[i][0];
+						newPos[1] = pos[1] + direction[i][1];
+						robot(1, newPos);
+					}
+				}
+			}
+		}
+		else(numRoute > 1)
+		{
+			for(size_t i = 0; i < 4; i++)
+			{
+				if(passable[i])
+				{
+					passable[i] = false;
+					int newPos[2];
+					newPos[0] = pos[0] + direction[i][0];
+					newPos[1] = pos[1] + direction[i][1];
+					robot(1, newPos);
+				}
+			}
+		}
+	}else if(pid > 0)
+	{
+		wait();
+		//把child給的訊息印出
 	}
 	
-	else if ( pid == 0 ) { // create odd numbers of child process (a half of full binary tree)
-
-		if ( ++count < process_number )
-			createProcess(count, process_number);
-	}
-
-	return pid;
 }
