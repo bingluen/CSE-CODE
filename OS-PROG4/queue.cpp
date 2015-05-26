21f95b6e3ade8569e3dbe164172dfc5a08bc9163
Erickson Juang <a820224@gmail.com>
2015-05-27 02:22:59 +0800

    modify output format

--- OS-PROG4/queue.cpp
+++ OS-PROG4/queue.cpp
@@ -1,25 +1,17 @@
 #include "Queue.h"
 ostream &operator<<(ostream &stream, const Queue &queue)
 {
-	stream << "("
-		<< setw(5) << "id, "
-		<< setw(5) << "arrival, "
-		<< setw(5) << "burst, "
-		<< setw(5) << "start, "
-		<< setw(5) << "end, "
-		<< setw(5) << "waiting, "
-		<< setw(5) << "execute"
-		<< ")" << endl;
+	stream << "( "
+		<< setw(5) << "id" << ", "
+		<< setw(5) << "start" << ", "
+		<< setw(5) << "end"
+		<< " )" << endl;
 	for (size_t i = 0; i < queue.queueSize(); i++)
 	{
 		stream << "( "
 			<< setw(5) << queue.getQueue(i).id << ", "
-			<< setw(5) << queue.getQueue(i).arrival << ", "
-			<< setw(5) << queue.getQueue(i).burst << ", "
 			<< setw(5) << queue.getQueue(i).start << ","
-			<< setw(5) << queue.getQueue(i).end << ", "
-			<< setw(5) << queue.getQueue(i).waiting << ", "
-			<< setw(5) << queue.getQueue(i).timeRequire
+			<< setw(5) << queue.getQueue(i).end
 			<< " )" << endl;
 	}
 
