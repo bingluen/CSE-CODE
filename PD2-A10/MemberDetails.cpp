35c9093db76b0881521de3731fb6d7ce2b65402d
Erickson Juang <a820224@gmail.com>
2015-06-05 03:04:02 +0800

    implement memberdetail

--- PD2-A10/MemberDetails.cpp
+++ PD2-A10/MemberDetails.cpp
@@ -28,3 +28,60 @@ int MemberDetails::inputAnInteger( int begin, int end )
    else
       return -1;
 }
+
+void MemberDetails::execute(Vector< Member >::iterator it)
+{
+	cout << "1. Name: " << it->getName() << endl;
+	cout << "2. Email address: " << it->getEmail() << endl;
+	cout << "3. Phone number: " << it->getPhone() << endl;
+	cout << "4. ID Number: " << it->getIdNumber() << endl;
+	cout << "5. Password: " << it->getPassword() << endl;
+
+	int choice;
+	do { cout << "Which one do you want to modify (0 -  not modify )?";  } while (choice = inputAnInteger(0, 5));
+
+	if (choice == 0)
+		return;
+
+	bool loopFlag = true;
+
+	while (loopFlag)
+	{
+		cout << "Enter correct data: " << endl;
+		string input;
+		cin.ignore();
+		getline(cin, input);
+
+		switch (choice) {
+		case 1:
+			it->setName(input);
+			loopFlag = false;
+			break;
+		case 2:
+			loopFlag = memberDatabase.existingEmail(input);
+			if(!loopFlag)
+				it->setEmail(input);
+			else
+				cout << "An account already exits with the email address !" << endl;
+			break;
+		case 3:
+			it->setPhone(input);
+			loopFlag = false;
+			break;
+		case 4:
+			loopFlag = memberDatabase.existingId(input);
+			if (!loopFlag)
+				it->setIdNumber(input);
+			else
+				cout << "An account already exists with the ID number !" << endl;
+			break;
+		case 5:
+			it->setPassword(input);
+			loopFlag = false;
+			break;
+		}
+	}
+
+	cout << "Successful!" << endl;
+	
+}
