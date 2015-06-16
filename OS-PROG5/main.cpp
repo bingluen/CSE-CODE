a7737bbfe7164ee5bcbfd9f8fbcc89c23522c1de
Erickson Juang <a820224@gmail.com>
2015-06-16 21:04:46 +0800

    finish

--- OS-PROG5/main.cpp
+++ OS-PROG5/main.cpp
@@ -14,6 +14,7 @@ const unsigned short int MATERIAL_BATTERY = 1;
 const unsigned short int MATERIAL_SENSOR = 2;
 const unsigned short int MATERIAL_WIFI = 3;
 const unsigned short int MATERIAL_CRAWLER = 4;
+const char MATERIAL_NAME[][8] = {"Battery", "Sensor", "Wifi", "Carwler"};
 
 const unsigned short int IDENTITY_PRODUCER = 0;
 const unsigned short int IDENTITY_DISPACHER = 1;
@@ -28,7 +29,7 @@ typedef struct {
 
 
 typedef struct {
-	unsigned short int id;
+	size_t id;
 	unsigned short int identity;
 	unsigned short int heldMaterials;
 	unsigned short int numOutput;
@@ -37,7 +38,7 @@ typedef struct {
 
 /* Function prototype */
 void basic();
-void advance();
+//void advance();
 void *threading(void*);
 void producer(Thread*);
 void dispacher(Thread*);
@@ -50,21 +51,32 @@ int numProduct = 0;
 
 int main(int argc, char *argv[])
 {
-	switch(atoi(argv[1]))
+
+	if(argc > 1)
 	{
-		case 2:
-			advance();
-			break;
-		case 1:
-		default:
-			basic();
+		switch(atoi(argv[1]))
+		{
+			case 2:
+				//advance();
+				break;
+			case 1:
+			default:
+				basic();
+				break;
+		}
+	} else {
+		basic();
 	}
+	
 
 	return 0;
 }
 
 void basic()
 {
+
+	//cout << "[Debug]" << "準備thread" << endl;
+
 	/* 5個thread的資料 */
 	Thread thread[5];
 	pthread_t thread_id[5];
@@ -75,6 +87,7 @@ void basic()
 	sem_init(&threadSem[0], 0, 0);
 	thread[0].identity = IDENTITY_DISPACHER;
 	thread[0].id = 0;
+	thread[0].heldMaterials = 0;
 	//Producer
 	for(size_t i = 1; i < 5; i++)
 	{
@@ -85,13 +98,34 @@ void basic()
 		thread[i].numOutput = 0;
 	}
 
+	/* thread 設定印出 
+	for(size_t i = 0; i < 5; i++)
+	{
+		cout << "[id = " << thread[i].id << "] identity = " << thread[i].identity 
+		<< " ,heldMaterials = " << thread[i].heldMaterials
+		<< " ,numOutput = " << thread[i].numOutput << endl;
+	}*/
+
+	//cout << "[Debug]" << "產生thread" << endl;
 	/* create thread */
 	for(size_t i = 0; i < 5; i++)
 	{
 		pthread_attr_init(thread_attr+i);
-		pthread_create(thread_id+i, thread_attr+i, threading, reinterpret_cast<void *> (&thread[i]));
+		pthread_create(thread_id+i, thread_attr+i, threading, reinterpret_cast<void *> (thread+i));
 	}
 
+	/* thread 設定印出 
+	for(size_t i = 0; i < 5; i++)
+	{
+		cout << "[id = " << thread[i].id << "] identity = " << thread[i].identity 
+		<< " ,heldMaterials = " << thread[i].heldMaterials
+		<< " ,numOutput = " << thread[i].numOutput << endl;
+	}*/
+
+	/* open dispacher */
+	sem_post(&threadSem[0]);
+
+	//cout << "[Debug]" << "等待thread" << endl;
 	for(size_t i = 0; i < 5; i++)
 	{
 		pthread_join(*(thread_id+i), NULL);
@@ -100,8 +134,8 @@ void basic()
 
 void* threading(void* threadContext)
 {
-	Thread *thread = ( reinterpret_cast<Thread* > (&threadContext) );
-
+	Thread *thread = ( reinterpret_cast<Thread* > (threadContext) );
+	
 	switch(thread->identity)
 	{
 		case IDENTITY_PRODUCER:
@@ -115,8 +149,11 @@ void* threading(void* threadContext)
 
 void producer(Thread* producerThread)
 {
-	while(numProduct < 40) {
+	while(true) {
 		sem_wait(&threadSem[producerThread->id]);
+
+		if (numProduct >= 40)
+			break;
 
 		//把東西取走
 		for(size_t i = 0; i < NUM_TYPE_MATERIAL; i++)
@@ -126,8 +163,30 @@ void producer(Thread* producerThread)
 		producerThread->numOutput++;
 		numProduct++;
 
+		cout << "Producer(" 
+			<< MATERIAL_NAME[producerThread->heldMaterials - 1] 
+			<< ") : OK, " << producerThread->numOutput << " Robot(s), Total: " 
+			<< numProduct << " Robot(s)." << endl;
+
 		//打開dispacher 的 sem
-		sem_post(&threadSem[0]);
+		//只有在numProduct < 40 才開
+		if(numProduct < 40)
+		{
+			sem_post(&threadSem[0]);
+		} else {
+		//若 numProduct == 40
+		//同時喚醒所有block的thread 讓他們離開
+			for(size_t i = 0; i < 5; i++)
+			{
+				int semVal;
+				sem_getvalue(&threadSem[i], &semVal);
+				if(semVal >= 0)
+				{
+					sem_post(&threadSem[i]);
+				}
+			}
+		}
+		
 		
 	}
 
@@ -136,8 +195,11 @@ void producer(Thread* producerThread)
 void dispacher(Thread* dispacherThread)
 {
 	srand(time(NULL));
-	while(numProduct < 40) {
+	while(true) {
 		sem_wait(&threadSem[dispacherThread->id]);
+
+		if (numProduct >= 40)
+			break;
 
 		//隨機放上東西
 		for(size_t i = 0; i < 3;)
@@ -146,6 +208,7 @@ void dispacher(Thread* dispacherThread)
 			if(!desk.material[j = rand()%4]) {
 				desk.material[j] = true;
 				i++;
+				cout << "Dispacher: " << MATERIAL_NAME[j] << endl;
 			}
 		}
 
